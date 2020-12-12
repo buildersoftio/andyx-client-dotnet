582536091af831107b34f9ee312333f8eb7f3f1d
Enes Hoxha <eneshoxha@outlook.com>
2020-12-12 19:04:52 +0100

    Added NodeProvider, Added Events for NodeReaderService

---
+++ src/net/andy-x-client-lib/Buildersoft.Andy.X.Client/Reader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buildersoft.Andy.X.Client
+{
+    public partial class Reader<TEntity> where TEntity : new()
+    {
+
+    }
+}
---
+++ src/net/andy-x-client-lib/Buildersoft.Andy.X.Client/Services/NodeReaderService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buildersoft.Andy.X.Client
+{
+    public partial class Reader<TEntity> where TEntity : new()
+    {
+        private class NodeReaderService
+        {
+            private readonly HubConnection _connection;
+
+            public event Action<ReaderConnectedArgs> ReaderConnected;
+            public event Action<ReaderDisconnectedArgs> ReaderDisconnected;
+            public event Action<MessageReceivedArgs> MessageReceived;
+        }
+    }
+}
