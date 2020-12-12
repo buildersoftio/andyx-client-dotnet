40fce9fa11a8e1b58b6ebea122ec065e28031bc4
Enes Hoxha <eneshoxha@outlook.com>
2020-12-12 19:10:38 +0100

    Implemented NodeReaderService

--- src/net/andy-x-client-lib/Buildersoft.Andy.X.Client/Services/NodeReaderService.cs
+++ src/net/andy-x-client-lib/Buildersoft.Andy.X.Client/Services/NodeReaderService.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Buildersoft.Andy.X.Client
 {
@@ -14,6 +16,57 @@ namespace Buildersoft.Andy.X.Client
             public event Action<ReaderConnectedArgs> ReaderConnected;
             public event Action<ReaderDisconnectedArgs> ReaderDisconnected;
             public event Action<MessageReceivedArgs> MessageReceived;
+
+            private readonly ILogger _logger;
+
+            public NodeReaderService(NodeProvider nodeProvider)
+            {
+                _logger = nodeProvider
+                    .GetAndyXClient()
+                    .GetAndyXOptions()
+                    .Logger
+                    .GetLoggerFactory()
+                    .CreateLogger<NodeReaderService>();
+
+                _connection = nodeProvider.GetHubConnection();
+
+                _connection.On<ReaderConnectedArgs>("ReaderConnected", connectedArgs => ReaderConnected?.Invoke(connectedArgs));
+                _connection.On<ReaderDisconnectedArgs>("ReaderDisconnected", disconnected => ReaderDisconnected?.Invoke(disconnected));
+                _connection.On<MessageReceivedArgs>("MessageReceived", received => MessageReceived?.Invoke(received));
+            }
+
+            public async Task ConnectAsync()
+            {
+                await _connection.StartAsync().ContinueWith(task =>
+                {
+                    if (task.Exception != null)
+                    {
+                        _logger.LogError($"Connection to Andy X Node failed, details {task.Exception.Message}");
+                    }
+                });
+            }
+
+            public async Task CloseConnectionAsync()
+            {
+                await _connection.StopAsync().ContinueWith(task =>
+                {
+                    if (task.Exception != null)
+                    {
+                        _logger.LogError($"Diconnection from Andy X Node failed, details {task.Exception.Message}");
+                    }
+                });
+            }
+
+
+            /// <summary>
+            /// Send to Andy X Node - Message as acknowledged
+            /// </summary>
+            /// <param name="message">MessageAckDetail type</param>
+            /// <returns></returns>
+            public async Task AcknowledgeMessage(MessageAckDetail message)
+            {
+                await _connection.SendAsync("AcknowledgeMessage", message);
+            }
         }
     }
 }
