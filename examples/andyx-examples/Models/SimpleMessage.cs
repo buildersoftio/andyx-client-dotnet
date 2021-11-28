d7c01d8f703f703a3337f63c5ddfd020ad478c8b
Enes Hoxha <eneshoxha@outlook.com>
2021-11-28 23:53:47 +0100

    Add Andy X Client example for .net

---
+++ examples/andyx-examples/Models/SimpleMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace andyx_examples.Models
+{
+    public class SimpleMessage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ examples/andyx-examples/SimpleProducer.cs
@@ -0,0 +1,29 @@
+﻿using Andy.X.Client;
+using andyx_examples.Models;
+
+namespace andyx_examples
+{
+    public class SimpleProducer
+    {
+        private readonly Producer<SimpleMessage> producer;
+        public SimpleProducer()
+        {
+            XClient client = new XClient("https://localhost:9001");
+
+            producer = new Producer<SimpleMessage>(client)
+                .Name("simple-producer")
+                .Component("simple")
+                .Topic("simple-message")
+                .RetryProducing(true)
+                .BuildAsync().Result as Producer<SimpleMessage>;
+
+            producer.OpenAsync().Wait();
+        }
+
+        public void ProduceSimpleMessages(int id, string name, string message)
+        {
+            var simpleMessage = new SimpleMessage() { Id = id, Name = name, Message = message };
+            producer.Produce(simpleMessage);
+        }
+    }
+}
